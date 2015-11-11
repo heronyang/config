832e649a5c40dc0d2fac8c586baf54e99a89e0b4
Heron Yang <heron.yang.tw@gmail.com>
2015-11-11 13:51:58 +0800

    update after finish mac test

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Environment Configuration Tool
 
 # Abstract
-Environment Configuration Tool (ECT) helps developers or system administrators to set up a new terminal environment based on the person's preference. That is, one can list the applications to install, and locate his/her own .rc files, then the tool will do the rest.
+Environment Configuration Tool (ECT) helps developers or system administrators to set up a new terminal environment based on the person's preference. That is, one can list the packages to install, and locate his/her own .rc files, then the tool will do the rest.
 
 # Dependency
 
@@ -13,7 +13,7 @@ Make sure [Mac Ports](https://www.macports.org/) is installed; otherwise, instal
 
 ## Write Your Own config.json
 
-In config.json file, you can name the list of applications you want to install. There will be the input arguments for your package installation tool, such as `apt-get install args`. Sample file is provided in this repo.
+In config.json file, you can name the list of packages you want to install. There will be the input arguments for your package installation tool, such as `apt-get install args`. Sample file is provided in this repo.
 
 # Run
 
--- run.py
+++ run.py
@@ -1,16 +1,55 @@
 #!/usr/bin/python
 
+import os, platform
 import json
 import argparse
 
+from IPython.core.debugger import Tracer
+
+from pprint import pprint
+from subprocess import call
+
+OS = os.name
+SYSTEM = platform.system()
+CONFIG_FILE_DEFAULT_PATH = "config.json"
+
 parser = argparse.ArgumentParser(description="Environment Configuration Tool")
-parser.add_argument('--input', type=argparse.FileType('r'))
+parser.add_argument('-i', '--input',
+        help="set filepath of the config file (default to " + CONFIG_FILE_DEFAULT_PATH +")",
+        type=argparse.FileType('r'),
+        default=CONFIG_FILE_DEFAULT_PATH)
+args = parser.parse_args()
+
+def permission_check():
+    if os.getuid() != 0:
+        print 'Please execute with root pervilege.'
+        os._exit(1)
 
 def read_config_file():
-    with open('config.json') as config_file:
+    print '> read confie file from: ' + args.input.name
+    with args.input as config_file:
         config = json.load(config_file)
-    pprint(config)
+
+    return config
+
+def install_packages(config):
+# Mac OS X
+    if SYSTEM == 'Darwin':
+        install_packages(config)
+
+def install_packages(config):
+    print '> your deployeed os: ' + SYSTEM
+    call(["port", "install"] + config["packages"])
 
 if __name__ == "__main__":
-    print parser
-    read_config_file()
+
+# setup
+    permission_check()
+
+    config = read_config_file()
+
+# run
+    install_packages(config)
+
+# end
+    print 'done.'
